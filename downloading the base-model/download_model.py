e2cd766853997b89e9fd405b063b6b0d17704334
shaheennabi <ishaheennabi333@gmail.com>
2026-04-24 04:51:01 -0700

    update

--- downloading the base-model/download_model.py
+++ downloading the base-model/download_model.py
@@ -1,18 +1,23 @@
 import os
 from huggingface_hub import snapshot_download
-from pathlib import Path
-## downloading the model from huggingface
 
-def download_model(repo_id, local_dir):
-    
-
-    if os.path.exists(local_dir):
-        downloaded_path = snapshot_download(repo_id=repo_id, local_dir=local_dir, allow_patterns=["*config.json", "*model.safetensors", "*tokenizer.json", "*tokenizer-config.json"], ignore_patterns=["vocab.json", "merges.txt", "generation_config.json", "readme.md", "LICENSE", ".gitattributes"])
-    else:
-        local_dir = os.makedirs(local_dir)
-        downloaded_path = snapshot_download(repo_id=repo_id, local_dir=local_dir, allow_patterns=["*config.json", "*model.safetensors", "*tokenizer.json", "*tokenizer-config.json"], ignore_patterns=["vocab.json", "merges.txt", "generation_config.json", "readme.md", "LICENSE", ".gitattributes"])
-
-    return downloaded_path
+def downlaod_model(repo_id, local_dir):
+    os.makedirs(local_dir, exist_ok=True)
 
 
-download_model(repo_id="Qwen/Qwen3-0.6B-Base", local_dir="qwen")
+    downlaod_path = snapshot_download(repo_id=repo_id,
+                    local_dir=local_dir,
+                    allow_patterns=[
+                        "config.json",
+                        "model.safetensors",
+                        "tokenizer.json",
+                        "tokenizer_config.json",
+                    ])
+    return downlaod_model
+
+
+
+downlaod_model(
+    repo_id="Qwen/Qwen3-0.6B-Base",
+    local_dir="qwen",
+    )
---
+++ logger.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import logging
+from pathlib import Path
+
+folder = Path("logger_folder")
+folder.mkdir(parents=True, exist_ok=True)
+
+log_filename = folder / f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(log_filename),
+        logging.StreamHandler()
+    ]
+)
+
+print("hello world")
+logging.info("hello printed")
